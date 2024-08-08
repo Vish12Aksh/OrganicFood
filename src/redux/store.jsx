94efdb58786f7e746496b12fd86a336440acac30
VISHANT KUMAR <vishantkumar7739@outlook.com>
2024-08-08 02:14:31 -0700

    Delete src directory

--- src/redux/store.jsx
+++
@@ -1,9 +0,0 @@
-// import card from '@material-tailwind/react/theme/components/card'
-import { configureStore } from '@reduxjs/toolkit' 
-import cardSlice from './cardSlice'
-
-export const store = configureStore({
-  reducer: {
-    card :cardSlice
-  },
-}) 
